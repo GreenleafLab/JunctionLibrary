fcd5fbbf83e463e3296816c386bebe9ed5c1f668
sjkdenny <sjkdenny@gmail.com>
2014-09-15 19:11:22 -0700

    included new receptors and loops

--- globalvars.py
+++ globalvars.py
@@ -32,15 +32,39 @@ class Parameters():
         
         
         self.loopDict = {'goodLoop': 'GGAA',
-                         'badLoop': 'GAAA'}
+                         'badLoop': 'GAAA',
+                         'KL1': 'AAGATTCCA',
+                         'KL2': 'AAGATTCA',
+                         'KL3': 'AAGATTA',
+                         'KL4': 'ACACAA',
+                         'KL5': 'AAGATA',
+                         'KL6': 'ACACAGGA'}
+        """
+        loops: KL1-4 are designed to hybridize to R2-receptor that binds GGAA tetraloop.
+        With A's added to either side.
+        """
+        
+        
         self.receptorDict   = ({'R1':('TATGG', 'CCTAAG'),
-                                'KL1': ('', ''),
-                                'KL2': ('', ''),
-                                'KL3': ('', ''),
-                                'KL4': ('', ''),
-                                'KL5': ('', ''),
-                                'KL6': ('', '')
+            'R1_2':('', 'CCTAAG'),
+                                'KL1': ('AGGTCGGA', 'A'),
+                                'KL2': ('AU', 'AGGTCGGA'),
+                                'KL3': ('AU', 'AGTCGGA'),
+                                'KL4': ('AU', 'AGGTCGA'),
+                                'KL5': ('AU', 'AGTCGA'),
+                                'KL6': ('A', 'AGGTCGGA'),
+                                'KL7': ('A', 'AGTCGGA'),
+                                'KL8': ('A', 'AGGTCGA'),
+                                'KL9    ': ('A', 'AGTCGA'),
                                     })
+        """
+        receptors KL1 through KL9 are different binding parters of flow piece 3.
+        R1_2 is different binding parnter to flow piece 2. Needs different tectoBase.
+        
+        R1_2_base = ('CTAG', 'CTAG')
+        R1_2 = ('ATGG', 'CCTAAGTC')
+        
+        """
 
         self.tectoBase = ('CTAGGA', 'TCCTAG')
         
