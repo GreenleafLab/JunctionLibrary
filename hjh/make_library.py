e944753e0152ba75dd9ae01ec63f0b4973117ff7
sjkdenny <sjkdenny@gmail.com>
2015-03-31 12:26:04 -0700

    changing a bunhc of stuff to find good flanking base pairs

--- hjh/make_library.py
+++ hjh/make_library.py
@@ -67,7 +67,11 @@ def findInitialDistance(allSeqs, indx):
 
 #parse command line arguments
 args = parser.parse_args()
-if args.out_file is None: args.out_file = (os.path.splitext(args.map_file)[0] + '.library').replace(',', '')
+if args.out_file is None:
+    if args.junction_sequence_file is None:
+        args.out_file = (os.path.splitext(args.map_file)[0] + '.library').replace(',', '')
+    else:
+        args.out_file = (os.path.splitext(args.junction_sequence_file)[0] + '.library').replace(',', '')
 if args.seq_params_file is None:
     args.seq_params_file = os.path.expanduser('~/JunctionLibrary/seq_params/seq_params.txt')
 
@@ -85,7 +89,7 @@ else:
     for motif in expt_params.loc[:, 'junction']:
         junctionSeqs = junctionSeqs.append(Junction(tuple(motif.split(','))).sequences, ignore_index=True)
     expt_params.loc[:, 'junction'] = ['defunct'] +[np.nan]*(len(expt_params)-1)
-junction = Junction(sequences=junctionSeqs.loc[:, ['side1', 'side2']])
+
 
 # iterate through all
 cols = TectoSeq(seq_params, expt_params.loc[0]).params.index
@@ -101,6 +105,7 @@ with open(args.out_file + '.pkl', 'wb') as output:
         params = pd.Series(params, index=expt_params.columns.tolist())
       
         # if up, junction remains the same. else switch sides
+        junction = Junction(sequences=junctionSeqs.loc[:, ['side1', 'side2']])
         if params.side == 'up':
             pass
         elif params.side == 'down':
@@ -124,6 +129,19 @@ with open(args.out_file + '.pkl', 'wb') as output:
 # check if successful secondary structure        
 allSeqs.loc[:, 'ss'] = hjh.tecto_assemble.getAllSecondaryStructures(allSeqs.loc[:, 'tecto_sequence'])
 
+#check if any module was successful
+numberFlankingBasepairs = 2
+allSeqs.loc[:, 'numberFlanking'] = numberFlankingBasepairs
+allSeqs.loc[:, 'no_flank'] = ['_'.join([allSeqs.loc[loc, 'tecto_object'].junction['side1'][numberFlankingBasepairs:-numberFlankingBasepairs],
+                                       allSeqs.loc[loc, 'tecto_object'].junction['side2'][numberFlankingBasepairs:-numberFlankingBasepairs]]) for loc in allSeqs.index] 
+allSeqs.loc[:, 'flank'] = ''
+for loc in allSeqs.index:
+    if allSeqs.loc[loc, 'side'] == 'up':
+        side = 'side1'
+    else:
+        side = 'side2'
+    allSeqs.loc[loc, 'flank'] = allSeqs.loc[loc, 'tecto_object'].junction[side][:numberFlankingBasepairs] + allSeqs.loc[loc, 'tecto_object'].junction[side][-numberFlankingBasepairs:]
+
 workerPool = multiprocessing.Pool(processes=numCores)
 indices = np.array_split(allSeqs.index.tolist(), numCores)
 allSeqSub = workerPool.map(hjh.tecto_assemble.getSecondaryStructureMultiprocess,
@@ -133,29 +151,97 @@ workerPool.close(); workerPool.join()
 allSeqs = pd.concat(allSeqSub) 
 allSeqs.drop(['tecto_object'], axis=1).to_csv(args.out_file+'.txt', sep='\t')
 
-
-
-#check if any module was successful
-allSeqs.loc[:, 'no_flank'] = ['_'.join([allSeqs.loc[loc, 'tecto_object'].junction['side1'][2:-2],
-                                       allSeqs.loc[loc, 'tecto_object'].junction['side2'][2:-2]]) for loc in allSeqs.index] 
-flanks = np.unique([allSeqs.loc[loc, 'tecto_object'].junction['side1'][:2] + allSeqs.loc[loc, 'tecto_object'].junction['side1'][-2:] for loc in allSeqs.index])
-
-iterables = [['A_', 'G_', 'C_', 'U_', '_'], expt_params.length.dropna(), expt_params.offset.dropna().values, expt_params.side.dropna().values]
+flanks = np.unique(allSeqs.loc[:, 'flank'])
+no_flanks = [f for f in np.unique(allSeqs.loc[:, 'no_flank']) if f[0] != '_']
+iterables = [no_flanks, expt_params.length.dropna(), expt_params.offset.dropna().values, expt_params.side.dropna().values]
 index = pd.MultiIndex.from_product(iterables, names=['no_flank', 'length', 'offset', 'side'])
 allsuccess = pd.DataFrame(index=flanks, columns=index)
 
 for loc in allSeqs.index:
-    if loc%10 == 0: print loc
-    index = allSeqs.loc[loc, 'tecto_object'].junction['side1'][:2] + allSeqs.loc[loc, 'tecto_object'].junction['side1'][-2:]
+    #if loc%10 == 0: print loc
+    index = allSeqs.loc[loc, 'flank']
     no_flank = allSeqs.loc[loc, 'no_flank']
     if allSeqs.loc[loc, 'side'] == 'down':
         no_flank = no_flank[::-1]
-    columns = (no_flank,
-               allSeqs.loc[loc, 'length'],
-               allSeqs.loc[loc, 'offset'],
-               allSeqs.loc[loc, 'side'])
-    allsuccess.loc[index, columns] = allSeqs.loc[loc, 'ss_correct']
+    if loc%100==0:
+        print loc, no_flank
+    
+    if no_flank in no_flanks:
+        columns = (no_flank,
+                   allSeqs.loc[loc, 'length'],
+                   allSeqs.loc[loc, 'offset'],
+                   allSeqs.loc[loc, 'side'])
+        allsuccess.loc[index, columns] = allSeqs.loc[loc, 'ss_correct']
+allsuccess.to_csv(args.out_file+'.success.mat', sep='\t')
+
+allsuccessTrimmed = allsuccessAll.dropna()
+worksInAll = pd.Series(index=allsuccessTrimmed.index, data=[allsuccessTrimmed.loc[loc].values.astype(bool).sum() for loc in allsuccessTrimmed.index])
+worksInAll.sort()
+
+no_flanks = ['A_', 'G_', 'C_', 'U_'] + [''.join(x)+'_' for x in itertools.product('ACGU', 'ACGU')]
+worksInAll = pd.DataFrame(index=allsuccessTrimmed.index, columns = no_flanks)
+for seq in no_flanks:
+    worksInAll.loc[:, seq]=[allsuccessTrimmed.loc[loc, seq].values.astype(bool).sum() for loc in allsuccessTrimmed.index]
+
+plt.figure(figsize=(20,20))
+a = worksInAll.loc[(worksInAll >=12).all(axis=1), 'A_'].copy()
+a.sort()
+ax = sns.heatmap(worksInAll.loc[a.index], square=True,
+                    linewidth=0.1, vmax=24, vmin=0,
+                    annot=True, fmt='d',
+                    cbar=False)
+ax.set_yticklabels(a.index.tolist(), rotation=0, fontsize=12)
+
+b = worksInAll.loc[a.index].loc[(worksInAll.loc[a.index, ['A_', 'U_']] == 24).all(axis=1)].sum(axis=1).copy()
+b.sort()
+c = worksInAll.loc[a.index].loc[(worksInAll.loc[a.index, ['G_', 'C_']] == 24).all(axis=1)].sum(axis=1).copy()
+c.sort()
+plt.figure(figsize=(10,10))
+ax = sns.heatmap(worksInAll.loc[pd.concat([b,c]).index], square=True,
+                    linewidth=0.1, vmax=24, vmin=0,
+                    annot=True, fmt='d',
+                    cbar=False)
+ax.set_yticklabels(pd.concat([b,c]).index.tolist(), rotation=0, fontsize=12)
+ax.set_xticklabels(no_flanks, rotation=90, fontsize=12)
+
+b = worksInAll.loc[a.index].loc[(worksInAll.loc[a.index, ['A_', 'C_']] == 24).all(axis=1)].sum(axis=1).copy()
+b.sort()
+c = worksInAll.loc[a.index].loc[(worksInAll.loc[a.index, ['G_', 'U_']] == 24).all(axis=1)].sum(axis=1).copy()
+c.sort()
+plt.figure(figsize=(10,10))
+ax = sns.heatmap(worksInAll.loc[pd.concat([b,c]).index], square=True,
+                    linewidth=0.1, vmax=24, vmin=0,
+                    annot=True, fmt='d',
+                    cbar=False)
+ax.set_yticklabels(pd.concat([b,c]).index.tolist(), rotation=0, fontsize=12)
+ax.set_xticklabels(no_flanks, rotation=90, fontsize=12)
+
+
+
+iterables = [no_flanks, expt_params.length.dropna(), expt_params.offset.dropna().values]
+index = pd.MultiIndex.from_product(iterables, names=['no_flank', 'length', 'offset'])
+allsuccessSmaller = pd.DataFrame(index = allsuccessTrimmed.index, columns=index)
+
+for loc in allsuccessSmaller.index:
+    for col in [name for name in allsuccessSmaller]:
+        allsuccessSmaller.loc[loc, col] = allsuccessTrimmed.loc[loc, col].any()
+plt.figure(figsize=(20,7))
+ax = sns.heatmap(allsuccessSmaller.loc[worksInAll.index].astype(float).transpose(), square=True,
+                    linewidth=0,
+                    cbar=False)
+
+plt.figure(figsize=(20,20))
+ax = sns.heatmap(allsuccessTrimmed.loc[worksInAll.loc[worksInAll >= 288].index].astype(float), square=True,
+                    linewidth=0,
+                    cbar=False)
+
+ax.set_xticklabels(ax.get_xticklabels(), fontsize=6)
+plt.savefig(args.out_file+'.pdf')
+
+
 sys.exit()
+
+
 
 worksInAll = pd.DataFrame(index = ['_'.join(x) for x in itertools.product(['GC', 'CG', 'UA', 'AU'], ['GC', 'CG', 'UA', 'AU'])],
                           columns=[allsuccess.index.tolist()])
@@ -175,104 +261,16 @@ for i, ind in enumerate(inds):
 
 sys.exit()
 
-# plot somehow
-plt.figure(figsize=(6,10))
-sns.heatmap(allsuccess.astype(int), square=True,
-                    
-                    cbar=False,)
-
-
-
-for flank1, flank2 in itertools.product(['GC', 'CG', 'UA', 'AU'], ['GC', 'CG', 'UA', 'AU']):
-    for i, length in enumerate([8.0, 9., 10., 11.]):
-        
-        print '%s\t%s'%(flank1, flank2,)
-        print allsuccess.loc[:,(flank1, flank2, '%3.1f'%length)].sum(axis=1)
+# get working flanks
+seqs = worksInAll.loc[worksInAll>=72].index.tolist()
+seqDict = {'A':'U', 'U':'A', 'G':'C', 'C':'G'}
+junctionMotifs = []
+for seq in seqs:
+    s = [''.join(x) for x in itertools.izip(list(seq), [seqDict[base] for base in seq])][::-1]
+    junctionMotifs.append( ','.join(s[:2] + ['B1,B1'] + s[-2:]))
     
-    offsets = [-2., -1., 0., 1., 2.]
-    fig = plt.figure(figsize=(5,7))
-    
-    gs = gridspec.GridSpec(4, 2, wspace=0.1, hspace=0.1)
-    for i, length in enumerate([8.0, 9., 10., 11.]):
-        
-        print '%s\t%s'%(flank1, flank2,)
-        print allsuccess.loc[:,(flank1, flank2, '%3.1f'%length)].sum(axis=1)
-        
-        ax1 = fig.add_subplot(gs[i,0])
-        ax2 = fig.add_subplot(gs[i,1])
-        
-        if i == 0:
-            ax1.set_title('side1')
-            ax2.set_title('side2')
-            ax1.annotate('%s %s'%(flank1, flank2), xy=(1, 1.1),
-                xytext=(0.8, 0.95), textcoords='figure fraction',
-                horizontalalignment='center', verticalalignment='top',
-                )
-            
-        if i == 3:
-            xticklabels=offsets
-            ax1.set_xlabel('offsets')
-            ax2.set_xlabel('offsets')
-        else:
-            xticklabels=['']
-            ax1.set_xlabel('')
-            ax2.set_xlabel('')
-        
-        sns.heatmap(allsuccess.loc[:,(flank1, flank2, '%3.1f'%length,)].iloc[:, np.arange(0, len(offsets)*2, 2)],
-                    ax=ax1,
-                    square=True,
-                    annot=True, fmt="d",
-                    cbar=False,
-                    yticklabels=allsuccess.index.tolist(),
-                    xticklabels=xticklabels)
-        
-        sns.heatmap(allsuccess.loc[:,(flank1, flank2, '%3.1f'%length,)].iloc[:, np.arange(1, len(offsets)*2, 2)],
-                    ax=ax2,
-                    square=True,
-                    annot=True, fmt="d",
-                    cbar=False,
-                    xticklabels=xticklabels)
+[worksInAll.loc[worksInAll >= 70].loc[np.array([seq[1:-1] for seq in worksInAll.loc[worksInAll >= 70].index.tolist()]) == dinuc] for dinuc in ['AA', 'UU', 'CC', 'GG']]
 
 
-# module success
-iterables = [['GC', 'CG', 'UA', 'AU'], ['GC', 'CG', 'UA', 'AU'], np.array([8.0, 9., 10., 11.,]).astype(str),
-    np.array([-2., -1., 0., 1., 2.]).astype(str), ['none', 'swap']]
-index = pd.MultiIndex.from_product(iterables, names=['flank1', 'flank2', 'length', 'offset', 'side'])
-allsuccess = pd.DataFrame(index = np.unique(allSeqs.loc[:, 'junction_seq_noflank']), columns=index)
 
-
-allsuccess.to_csv(filename, sep='\t', index=True, header=True)
-
-# laod again
-num = logSeqs.number.sum()
-tectoSeqs = ['']*num
-with open(args.out_file, 'rb') as input:
-    for j in range(num):
-
-        tectoSeqs[j] = pickle.load(input)
-        seq, ss = tectoSeqs[j].findSecondaryStructure(tectoSeq=tectoSeqs[j].params.tecto_sequence)
-        print '%d\t%s\t%s\t%s'%(j, tectoSeqs[j].params.junction_seq, ss, tectoSeqs[j].printVarnaCommand())
-
-# print number that worked per flanking sequence
-numSuccesful = pd.DataFrame(index=['GC', 'CG', 'UA', 'AU'], columns=['GC', 'CG', 'UA', 'AU'])
-for row, col in itertools.product(numSuccesful.index, numSuccesful.columns):
-    numSuccesful.loc[row, col] = np.sum([tectoSeq.isSecondaryStructureCorrect()[0] for tectoSeq in tectoSeqs[row][col]])
-
-
-numCores = 20
-f = functools.partial(findInitialDistance, allSeqs)
-workerPool = multiprocessing.Pool(processes=numCores) #create a multiprocessing pool that uses at most the specified number of cores
-x = workerPool.map(f, allSeqs.index)
-workerPool.close()
-workerPool.join()
-
-results = pd.concat([pd.DataFrame(x[i], columns=[i]).transpose() for i in  allSeqs.index])
-
-topology = 'B1'
-sns.lmplot('offset' , 'distance', results, hue='junction_seq', fit_reg=False, palette='Set1')
-plt.savefig(os.path.join(figDirectory, 'initial_distance.%s.pdf'%topology))
-sns.lmplot('offset' , 'distance', results,  palette='Set1', x_estimator=np.mean, fit_reg=False)
-plt.savefig(os.path.join(figDirectory, 'initial_distance.%s.mean.pdf'%topology))
-
-# colormap
 
