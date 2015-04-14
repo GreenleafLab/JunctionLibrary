8e68cf1870f89e778d1b6fbe784020f3b2d95363
sjkdenny <sjkdenny@gmail.com>
2015-04-14 13:43:07 -0700

    option to not check ss of loop

--- hjh/tecto_assemble.py
+++ hjh/tecto_assemble.py
@@ -6,25 +6,29 @@ import numpy as np
 from hjh.helix import Helix
 
 class TectoSeq():
-    def __init__(self, seq_params,  params=None, helix=None, junction=None, sequence=None, reverse_junction=None):
+    def __init__(self, seq_params=None,  params=None, helix=None, junction=None, sequence=None, reverse_junction=None):
+        if junction is not None:
+            # decide whether to flip junction or not
+            if reverse_junction is None: reverse_junction = False
+            if reverse_junction:
+                junction.index = junction.index[::-1]
+            self.junction = junction
+        
+        if seq_params is not None:
+            self.loop     = seq_params.loc[('loop', params.loop), 'loop']
+            self.receptor = seq_params.loc[('receptor', params.loc['receptor']),  ['side1', 'side2']]
+            self.helix    = helix
+            self.base     = seq_params.loc[('base', params.loc['base']),  ['side1', 'side2']]
+            self.adapters = seq_params.loc[('adapters', params.loc['adapters']),  ['side1', 'side2']]
+            self.sequence = sequence
 
-        self.loop     = seq_params.loc[('loop', params.loop), 'loop']
-        self.receptor = seq_params.loc[('receptor', params.loc['receptor']),  ['side1', 'side2']]
-        self.helix    = helix
-        self.base     = seq_params.loc[('base', params.loc['base']),  ['side1', 'side2']]
-        self.adapters = seq_params.loc[('adapters', params.loc['adapters']),  ['side1', 'side2']]
-        self.sequence = sequence
+            # save params in a series
+            self.params = params.append(self.returnInfo())
+            self.name = '_'.join(params.loc[['junction', 'length', 'offset']].astype(str))
+            
+
         
-        # decide whether to flip junction or not
-        if reverse_junction is None: reverse_junction = False
-        if reverse_junction:
-            junction.index = junction.index[::-1]
-        self.junction = junction
-        
-        # save params in a series
-        self.params = params.append(self.returnInfo())
-        self.name = '_'.join(params.loc[['junction', 'length', 'offset']].astype(str))
-        
+
         # if junction has a name, change params name
         if junction is not None:
             if 'name' in junction.index:
@@ -49,7 +53,7 @@ class TectoSeq():
         
         # if tectoSeq is defined, make everything the same color
         if tectoSeq is not None:
-            colormap = np.ones(len(tectoSeq))
+            colormap = np.zeros(len(tectoSeq))
         else:
             regions = self.getRegions()
             colormap = regions.copy()
@@ -75,13 +79,24 @@ class TectoSeq():
         seq, ss, energy = subprocess.check_output("echo %s | RNAfold"%tectoSeq, shell=True).split()
         return seq, ss
     
-    def printVarnaCommand(self, tectoSeq=None, name=None):
+    def printVarnaCommand(self, tectoSeq=None, name=None, indices=None, use_colormap=None):
         if name is None: name = 'test.png'
+        if use_colormap is None: use_colormap = True
+        
         varnaScript = '~/VARNAv3-92.jar'
         seq, ss = self.findSecondaryStructure(tectoSeq)
+        
         colormap = self.makeColormap(tectoSeq)
-        commandString = 'java -cp %s fr.orsay.lri.varna.applications.VARNAcmd -sequenceDBN "%s" -structureDBN "%s" -colorMap "%s" -colorMapStyle blue -spaceBetweenBases "0.75" -o %s'%(varnaScript, seq, ss, ';'.join(colormap.astype(str)), name)
-        return commandString, seq, ss
+        if indices is not None:
+            seq = ''.join(np.array(list(seq))[indices])
+            ss = ''.join(np.array(list(ss))[indices])
+            colormap = colormap[indices]
+        
+        if use_colormap:
+            commandString = 'java -cp %s fr.orsay.lri.varna.applications.VARNAcmd -sequenceDBN "%s" -structureDBN "%s" -colorMap "%s" -colorMapStyle blue -spaceBetweenBases "0.6" -drawBases False -o %s'%(varnaScript, seq, ss, ';'.join(colormap.astype(str)), name)
+        else:
+            commandString = 'java -cp %s fr.orsay.lri.varna.applications.VARNAcmd -sequenceDBN "%s" -structureDBN "%s" -spaceBetweenBases "0.6" -drawBases False -o %s'%(varnaScript, seq, ss, name)
+        return commandString
     
     def returnInfo(self):
         
@@ -94,21 +109,27 @@ class TectoSeq():
             params.loc['sequence']  = self.threadTogether(params.loc['tecto_sequence'], self.adapters.loc[['side1', 'side2']]).replace('U', 'T')
             params.loc['helix_one_length'] = len(self.helix.split.loc['side1', 'before'])
             # things that require junction had other settings (i.e. name and n_flank)
+        except AttributeError: pass
+        
+        try:
             params.loc['n_flank'] = int(self.junction.loc['n_flank'])
             params.loc['no_flank'] = '_'.join([self.junction.loc[side][params.loc['n_flank']:-params.loc['n_flank']] for side in ['side1', 'side2']])
             params.loc['flank'] = ''.join([self.junction.loc['side1'][:params.loc['n_flank']], self.junction.loc['side1'][-params.loc['n_flank']:]])
             #params.loc['junction'] = self.junction.loc['name']
-            
-        except AttributeError: pass
+        except (AttributeError, KeyError):
+            pass
+
         
         return params
     
-    def isSecondaryStructureCorrect(self, ss=None, numberFlankingBases=None):
+    def isSecondaryStructureCorrect(self, ss=None, numberFlankingBases=None, ssReceptor=None):
         regions = self.getRegions()
         if numberFlankingBases is None:
             numberFlankingBases = 1
         if ss is None:
             seq, ss = self.findSecondaryStructure()
+        if ssReceptor is None:
+            ssReceptor = True
         ss = pd.Series(list(ss), dtype=str)
         
         expectedSecondaryStructure = {'base':'(((((())))))',
@@ -127,14 +148,7 @@ class TectoSeq():
             isCorrect = False
             print '\tissue with base region. Expected/actual: %s\t%s'%(expectedSecondaryStructure['base'], ''.join(ss.loc[regions == 'base']) )
         
-        # receptor: assume tetraloop receptor
-        if ''.join(ss.loc[regions == 'receptor']) == expectedSecondaryStructure['receptor']:
-            pass
-        else:
-            isCorrect = False
-            print '\tissue with receptor region. Is it 11nt receptor? Expected/actual: %s\t%s'%(expectedSecondaryStructure['receptor'], ''.join(ss.loc[regions == 'receptor']))
-            
-        # receptor: assume tetraloop receptor
+        # helix: assume basepaired
         if helixSS == expectedSecondaryStructure['helix']:
             pass
         else:
@@ -151,6 +165,22 @@ class TectoSeq():
             isCorrect = False
             print '\tissue with junction region. Expected:/actual %s*()*%s\t%s'%(''.join(['(']*numberFlankingBases), ''.join([')']*numberFlankingBases), junctionSS)
             
+            
+        if ssReceptor:
+            # receptor: assume tetraloop receptor
+            if ''.join(ss.loc[regions == 'receptor']) == expectedSecondaryStructure['receptor']:
+                pass
+            else:
+                isCorrect = False
+                print '\tissue with receptor region. Is it 11nt receptor? Expected/actual: %s\t%s'%(expectedSecondaryStructure['receptor'], ''.join(ss.loc[regions == 'receptor']))
+            
+            # loop: assume unpaires
+            if ''.join(ss.loc[regions == 'loop']) == expectedSecondaryStructure['loop']:
+                pass
+            else:
+                isCorrect = False
+                print '\tissue with loop region.  Expected/actual: %s\t%s'%(expectedSecondaryStructure['loop'], ''.join(ss.loc[regions == 'loop']))
+            
         return isCorrect, junctionSS, ''.join(ss)
 
 def findTecto(params, junction, seq_params, locs):
@@ -160,7 +190,8 @@ def findTecto(params, junction, seq_params, locs):
         helix = Helix(seq_params.loc[('helix', params.helix)],
                       junction.findEffectiveJunctionLength(sequence=junction.sequences.loc[loc]),
                       params.offset, int(params.length))
-                
+        # make all NaNs into empty strings
+        #junction.sequences.loc[loc, np.isnan(junction.sequences.loc[loc])] = ''        
         tectoSeq = TectoSeq(seq_params, params, helix, junction.sequences.loc[loc])
         allSeqSub.loc[loc] = tectoSeq.params
         allSeqSub.loc[loc, 'tecto_object'] = tectoSeq
@@ -185,12 +216,14 @@ def getAllSecondaryStructures(tectoSeqs):
         secondaryStructures[i] = line.split()[2]
     return pd.Series(secondaryStructures, index=indices )
 
-def getSecondaryStructureMultiprocess(allSeqSub):
+def getSecondaryStructureMultiprocess(checkReceptor, allSeqSub):
 
     allSeqSub.loc[:, 'ss_correct'] = False
     allSeqSub.loc[:, 'junction_SS'] = ''
     for loc in allSeqSub.index:
-        ss_correct, junctionSS, ss = allSeqSub.loc[loc, 'tecto_object'].isSecondaryStructureCorrect(ss=allSeqSub.loc[loc, 'ss'], numberFlankingBases=allSeqSub.loc[loc, 'n_flank'],)
+        ss_correct, junctionSS, ss = allSeqSub.loc[loc, 'tecto_object'].isSecondaryStructureCorrect(ss=allSeqSub.loc[loc, 'ss'],
+                                                                                                    numberFlankingBases=allSeqSub.loc[loc, 'n_flank'],
+                                                                                                    ssReceptor=checkReceptor)
         allSeqSub.loc[loc, 'ss_correct'] = ss_correct
         allSeqSub.loc[loc, 'junction_SS'] = junctionSS
     
