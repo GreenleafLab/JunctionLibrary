56e52f501b952f2f6be9c81f3ce49efac46da6f1
sjkdenny <sjkdenny@gmail.com>
2017-09-06 14:24:35 -0700

    changed to be generalizeable: two options 'starting seq' and 'add seqs'

--- assemble_tecto_seqs.py
+++ assemble_tecto_seqs.py
@@ -18,14 +18,10 @@ from hjh import processing
 
 #set up command line argument parser
 parser = argparse.ArgumentParser(description="script for making library")
-parser.add_argument('-l', '--loops', help='loop sequences',
+parser.add_argument('-a', '--starting_seq', help='seed sequence, i.e. a loop. Must hve "seq" column.',
                     default='seq_params/loop_1.dat')
-parser.add_argument('-a', '--adapter_seqs', help='side1 and side2 of the adapter sequences',
-                    default='seq_params/adapters_1.dat')
-parser.add_argument('-b', '--base_seqs', help='side1 and side2 of the base sequence helix of tectoRNA',
-                    default='seq_params/base_1.dat')
-parser.add_argument('-r', '--receptor_seqs', help='side1 and side2 of the receptor sequence', required=True)
-parser.add_argument('-s', '--helix_seqs', help='side1 and side2 of the helix sequences', required=True)
+parser.add_argument('-b', '--add_seqs', nargs="+", help='list of filenames of sequences to add. '
+                    'All should have columns "side1" and "side2" of the adapter sequences')
 parser.add_argument('-out','--out_file', help='file to save output', required=True)
 
 
@@ -33,15 +29,13 @@ if __name__ == '__main__':
     args = parser.parse_args()
     
     # load seqs
-    adapter_seqs = processing.load_file(args.adapter_seqs)
-    base_seqs = processing.load_file(args.base_seqs)
-    receptor_seqs = processing.load_file(args.receptor_seqs)
-    helix_seqs = processing.load_file(args.helix_seqs)
-    loop_seqs = processing.load_file(args.loops)
+    loop_seqs = processing.load_file(args.starting_seq)
+    other_seqs = [processing.load_file(filename) for filename in args.add_seqs]
     
     # starting with the loop, thread together the sequential pieces of the tectoRNA
     new_seqs = loop_seqs
-    for add_seqs in [helix_seqs, receptor_seqs, base_seqs]:
+    new_seqs.loc[:, 'starting_seq'] = new_seqs.seq
+    for add_seqs in other_seqs:
         all_seqs = []
         for (idx1, row1), (idx2, row2) in itertools.product(new_seqs.iterrows(), add_seqs.iterrows()):
             seq = processing.thread_together(row1.seq, (row2.side1, row2.side2))
@@ -50,18 +44,7 @@ if __name__ == '__main__':
             all_seqs.append(seq_data)
         all_seqs = pd.concat(all_seqs, axis=1).transpose()
         new_seqs = all_seqs
-    # save seq without adapters
-    new_seqs.loc[:, 'tecto_sequence'] = new_seqs.seq
-    
-    # add adapter_seqs
-    all_seqs = []
-    for (idx1, row1), (idx2, row2) in itertools.product(new_seqs.iterrows(), adapter_seqs.iterrows()):
-        seq = processing.thread_together(row1.seq, (row2.side1, row2.side2))
-        seq_data = pd.concat([pd.Series({'seq':seq}),
-            row1.drop('seq'), row2.drop(['side1', 'side2'])])
-        all_seqs.append(seq_data)
-    new_seqs = pd.concat(all_seqs, axis=1).transpose()
-    
+
     # save
     new_seqs.to_csv(args.out_file, sep='\t', index=False)
     
